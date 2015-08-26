34243829377f04566d9ce7151c3feebf01879475
Zhang Ru <ru.zhang@outlook.com>
2015-08-26 12:22:24 +0800

    fix bugs

--- script.js
+++ script.js
@@ -554,6 +554,12 @@ var current_banner;
 
 window.onload=function(){
     banner_pic.style.height=browser_width*0.2785+'px';//重置banner高度以适应屏幕
+
+ 
+} 
+window.onresize=function(){
+    browser_width=parseInt(document.documentElement.clientWidth);
+    banner_pic.style.height=browser_width*0.2785+'px'; }
     //动画函数
     var num_count=0;
     var timer=setInterval(function(){
@@ -583,6 +589,7 @@ window.onload=function(){
     }
     //鼠标移除重启自动滚动
     banner_pic.onmouseout=function(){
+        current_banner=getBannerPage();
         num_count=current_banner;
         timer=setInterval(function(){
             banner_pic_lists[num_count%3].style.opacity=0;
@@ -613,11 +620,5 @@ window.onload=function(){
             }
         })
     }
- 
-} 
-window.onresize=function(){
-    browser_width=parseInt(document.documentElement.clientWidth);
-    banner_pic.style.height=browser_width*0.2785+'px'; }
-
 
 
